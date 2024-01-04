b75ca3cd13be663effdd035be249a3bc3a889c7b
ArpitaAgrahari <arpitaagrahari004@gmail.com>
2024-01-04 17:31:41 +0530

    Updated Commit

--- 01-Array_Min_Max.cpp
+++ 01-Array_Min_Max.cpp
@@ -1,1 +1,32 @@
-#
+#include<iostream>
+#include <climits>
+using namespace std;
+
+int getMin(int num,int n){
+    int min = INT_MAX;
+    for(int i = 0;i<n;i++){
+        if(num[i]<min){
+            min = num[i];
+        }
+    }
+    return min;
+}
+int getMax(int num,int n){
+    int max = INT_MIN;
+    for(int i = 0;i<n;i++){
+        if(num[i]> max){
+            max = num[i];
+        }
+    }
+    return max;
+}
+int main(){
+    int size;
+    cin>>size;
+    int num[10];
+    for(int i = 0;i<size;i++){
+        cin>>num[i];
+    }
+    cout<<"Maximum of array:"<< getMax[num,size] <<endl;
+    cout<<"Minimum of array:"<<getMin[num, size]<<endl;
+}
