aed99a90572c38fa94e4c90354821366f1604fa2
ArpitaAgrahari <arpitaagrahari004@gmail.com>
2024-01-05 23:08:45 +0530

    UPdated Commit

---
+++ 11-pratice_MoveAllZeroesToEnd.cpp
@@ -0,0 +1,22 @@
+#include<iostream>
+using namespace std;
+void printArray(int arr[],int size){
+    for(int i =0;i<size;i++){
+        cout<<arr[i]<<" ";
+    }
+}
+int shiftingZeroes(int arr[],int size){
+    int j = 0;
+    for(int i = 0;i<size;i++){
+        if(arr[i] != 0){
+            swap(arr[j],arr[i]);
+            j++;
+        }
+    }
+}
+int main(){
+    int arr[5] = {0,1,2,3,0};
+    printArray(arr,5);
+    shiftingZeroes(arr,5);
+    cout<<"Shifted Array: "<<shiftingZeroes(arr,5);
+}
