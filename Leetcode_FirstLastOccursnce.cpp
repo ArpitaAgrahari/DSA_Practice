86cf6b101a555e86254824fe4d24be4168d5e408
ArpitaAgrahari <arpitaagrahari004@gmail.com>
2024-01-05 19:47:11 +0530

    Updated Commit

--- Leetcode_FirstLastOccursnce.cpp
+++ Leetcode_FirstLastOccursnce.cpp
@@ -1,9 +1,10 @@
 #include<iostream>
 using namespace std;
 int firstOccurance(int arr[],int size,int key){
-    start = 0;
-    end = size-1;
-    mid = (start+end)/2;
+    int start = 0;
+    int end = size-1;
+    int mid = (start+end)/2;
+    int ans = -1;
     while(start<=end){
         if(arr[mid] == key){
             ans = mid;
@@ -17,25 +18,25 @@ int firstOccurance(int arr[],int size,int key){
     }
     return ans;
 }
-int lastOccurance(int arr[],int size,int key){
-    start = 0;
-    end = size-1;
-    mid = (start+end)/2;
-        while(start<=end){
-        if(arr[mid] == key){
-            ans = mid;
-            end = mid+1;
-        }else if(key>arr[mid]){
-            start = mid+1;
-        }else{
-            end = mid-1;
-        }
-        mid = (start+end)/2;
-    }
-    return ans;
-}
+// int lastOccurance(int arr[],int size,int key){
+//     start = 0;
+//     end = size-1;
+//     mid = (start+end)/2;
+//         while(start<=end){
+//         if(arr[mid] == key){
+//             ans = mid;
+//             end = mid+1;
+//         }else if(key>arr[mid]){
+//             start = mid+1;
+//         }else{
+//             end = mid-1;
+//         }
+//         mid = (start+end)/2;
+//     }
+//     return ans;
+// }
 int main(){
-    int arr[6] = {1,2,2,2,3,4};
-    cout<<"First Occurance of key is:"<<firstOccurance(arr,6,2) 
+    int arr[6] = {1,1,2,2,3,4};
+    cout<<"First Occurance of key is:"<<firstOccurance(arr,6,1);
     return 0;
 }
---
+++ tempCodeRunnerFile.cpp
@@ -0,0 +1,17 @@
+int lastOccurance(int arr[],int size,int key){
+//     start = 0;
+//     end = size-1;
+//     mid = (start+end)/2;
+//         while(start<=end){
+//         if(arr[mid] == key){
+//             ans = mid;
+//             end = mid+1;
+//         }else if(key>arr[mid]){
+//             start = mid+1;
+//         }else{
+//             end = mid-1;
+//         }
+//         mid = (start+end)/2;
+//     }
+//     return ans;
+// }
