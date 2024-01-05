517cef7dd4d2b097e7240380148f86f760d18833
ArpitaAgrahari <arpitaagrahari004@gmail.com>
2024-01-05 19:44:45 +0530

    Updated Commit

---
+++ Leetcode_FirstLastOccursnce.cpp
@@ -0,0 +1,41 @@
+#include<iostream>
+using namespace std;
+int firstOccurance(int arr[],int size,int key){
+    start = 0;
+    end = size-1;
+    mid = (start+end)/2;
+    while(start<=end){
+        if(arr[mid] == key){
+            ans = mid;
+            end = mid-1;
+        }else if(key>arr[mid]){
+            start = mid+1;
+        }else{
+            end = mid-1;
+        }
+        mid = (start+end)/2;
+    }
+    return ans;
+}
+int lastOccurance(int arr[],int size,int key){
+    start = 0;
+    end = size-1;
+    mid = (start+end)/2;
+        while(start<=end){
+        if(arr[mid] == key){
+            ans = mid;
+            end = mid+1;
+        }else if(key>arr[mid]){
+            start = mid+1;
+        }else{
+            end = mid-1;
+        }
+        mid = (start+end)/2;
+    }
+    return ans;
+}
+int main(){
+    int arr[6] = {1,2,2,2,3,4};
+    cout<<"First Occurance of key is:"<<firstOccurance(arr,6,2) 
+    return 0;
+}
