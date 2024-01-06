c5a605ec23dcddaa4688a69d0f1f8858ec4013b3
ArpitaAgrahari <arpitaagrahari004@gmail.com>
2024-01-06 12:35:12 +0530

    Updated Commit

--- 10-pratice_checkIfArrayIsSorted.cpp
+++ 10-pratice_checkIfArrayIsSorted.cpp
@@ -2,15 +2,15 @@
 using namespace std;
 int ifSortedArray(int arr[],int size){
     for(int i = 0;i<size-1;i++){
-        if(arr[i] > arr[i+1]){
+        if(arr[i] < arr[i+1]){
             return 1;
         }
     }
     return 0;
 }
 int main(){
-    int arr[1] = {100};
-    ifSortedArray(arr,1);
+    int arr[4] = {100,20,1,8};
+    ifSortedArray(arr,4);
     if(1){
         cout<<"Array is sorted.";
     }else{
