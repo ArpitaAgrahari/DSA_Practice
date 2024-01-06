1a0f81c54d1e91101d488eacba92f9d814e61a70
ArpitaAgrahari <arpitaagrahari004@gmail.com>
2024-01-06 10:50:30 +0530

    Updated Commit

--- 12_SelectionSorting.cpp
+++ 12_SelectionSorting.cpp
@@ -1,2 +1,17 @@
 #include<iostream>
 using namespace std;
+void selectionSorting(int arr,6){
+     for(int i = 0;i<size-1;i++){
+        int minIndex = 1;
+        for(int j = i+1;j<size;i++){
+            if(arr[j]<arr[minIndex]){
+                minIndex = j;
+            }
+        }
+        swap(arr[minIndex],arr[i]);
+     }
+}
+int main(){
+    int arr[6] = {3,0,9,4,7,2};
+    selectionSorting(arr,6);
+}
