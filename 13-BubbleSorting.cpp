b55d5c4203bad90b2587ab3623547d2a04d69941
ArpitaAgrahari <arpitaagrahari004@gmail.com>
2024-01-06 14:24:52 +0530

    Updated Commit

--- 13-BubbleSorting.cpp
+++ 13-BubbleSorting.cpp
@@ -1,6 +1,20 @@
 #include<iostream>
 using namespace std;
-
+void printArray(int arr[],int size){
+    for(int i = 0;i<size;i++){
+        cout<<arr[i]<<" ";
+    }
+}
+void sortingBubble(int arr[],int size){
+    for(int i = 0;i<size;i++){
+        if(arr[i]>arr[i+1]){
+            swap(arr[i],arr[i+1]);
+        }
+    }
+}
 int main(){
     int arr[6] = {9,4,3,7,1,0};
+    sortingBubble(arr,6);
+    cout<<"Sorted Array: ";
+    printArray(arr,6);
 }
