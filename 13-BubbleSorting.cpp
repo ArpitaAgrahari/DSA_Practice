cdf77887580f6e07de043469c042125283d9809d
ArpitaAgrahari <arpitaagrahari004@gmail.com>
2024-01-07 10:04:36 +0530

    Updated Commit

--- 13-BubbleSorting.cpp
+++ 13-BubbleSorting.cpp
@@ -5,6 +5,7 @@ void printArray(int arr[],int size){
         cout<<arr[i]<<" ";
     }
 }
+/*
 void sortingBubble(int arr[],int size){
     int start = 0;
     int end = size-1;
@@ -14,6 +15,15 @@ void sortingBubble(int arr[],int size){
         if(arr[i]<arr[i+1]){
             swap(arr[i],arr[i+1]);
     printArray(arr,6);
+        }
+    }
+*/
+void sortingBubble(int arr[],int size){
+    for(int i=0;i<size;i++){
+        for(int j=0;j<size;j++){
+            if(arr[j]>arr[j+1]){
+                swap(arr[j],arr[j+1]);
+            }
         }
     }
 }
