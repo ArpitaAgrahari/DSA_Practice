4cd3a7fa235e298475d0ead8bd47acadedc23236
ArpitaAgrahari <arpitaagrahari004@gmail.com>
2024-01-09 00:33:19 +0530

    Updated Commit

--- 14-StringArray.cpp
+++ 14-StringArray.cpp
@@ -1,10 +1,21 @@
 #include<iostream>
 using namespace std;
+
+int getLength(char name[]){
+    int count =0;
+    for(int i =0;name[i]!='\0',i++){
+        count++;
+    }
+    return 0;
+}
 int main(){
     char name[10];
     cout<<"Enter ur name:";
     cin>>name;
-    cout<<"Your name is: ";
+    cout<<"Your name is ";
     cout<<name<<endl;
-    
+
+    cout<<"The length of ur string is:";
+    cout<<getLength(char name)<<endl;
+
 }
