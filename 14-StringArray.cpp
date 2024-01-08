b9f77365fc21086f713b2a9422c16aac78d5c461
ArpitaAgrahari <arpitaagrahari004@gmail.com>
2024-01-09 00:48:24 +0530

    Updated Commit

--- 14-StringArray.cpp
+++ 14-StringArray.cpp
@@ -1,6 +1,14 @@
 #include<iostream>
 using namespace std;
 
+void reverse(char name[],int n){
+    int s = 0;
+    int e = n-1;
+    while(s<e){
+        swap(name[s++],name[n--]);
+        
+    }
+}
 int getLength(char name[]){
     int count =0;
     for(int i =0;name[i] != '\0';i++){
@@ -14,8 +22,10 @@ int main(){
     cin>>name;
     cout<<"Your name is ";
     cout<<name<<endl;
-
+    int len = getLength(name);
     cout<<"The length of ur string is:";
-    cout<<getLength(name)<<endl;
-
+    cout<<len<<endl;
+    reverse(name,len);
+    cout<<"The reverse of your string is: ";
+    cout<<name<<endl;
 }
