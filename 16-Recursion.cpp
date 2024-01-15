097e843adec22ff1f5085f3312499f8ad54a9fc6
ArpitaAgrahari <arpitaagrahari004@gmail.com>
2024-01-15 13:55:16 +0530

    Updated Commit

--- 16-Recursion.cpp
+++ 16-Recursion.cpp
@@ -31,3 +31,24 @@
 //     int ans = power(n);
 //     cout<<ans<<endl;
 // }
+
+ 
+//counting
+
+// #include<iostream>
+// using namespace std;
+// void print(int n){
+//     if(n==0)  {
+//         return ;
+//     }
+//     cout<<n<<endl;
+
+//     print(n-1);
+
+// }
+// int main(){
+//     int n;
+//     cin>>n;
+//     cout<<endl;
+//     print(n);
+// }
