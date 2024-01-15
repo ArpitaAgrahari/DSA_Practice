ab1936a2520a89ef3f33e846b85d9d5a4a835749
ArpitaAgrahari <arpitaagrahari004@gmail.com>
2024-01-15 13:44:01 +0530

    Updated Commit

---
+++ 16-Recursion.cpp
@@ -0,0 +1,33 @@
+//factorial uisng recursion
+
+// #include<iostream>
+// using namespace std;
+
+// int factorial(int n){
+//     if(n==0)  return 1;
+//     int base_Problem  = factorial(n-1);
+//     int Problem = n*base_Problem; 
+//     return Problem;
+// }
+// int main(){
+//     int n;
+//     cin>>n;
+//     int ans = factorial(n);
+//     cout<<ans<<endl;
+// }
+
+//Power of n of gievn integer
+
+// #include<iostream>
+// using namespace std;
+// int power(int n){
+//     if(n==0)  return 1;
+//     int Base_case = power(n-1);
+//     return 2*Base_case;
+// }
+// int main(){
+//     int n;
+//     cin>> n;
+//     int ans = power(n);
+//     cout<<ans<<endl;
+// }
