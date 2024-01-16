e19df52e9c0763d0d31e854f7cbc37b24bcc14eb
ArpitaAgrahari <arpitaagrahari004@gmail.com>
2024-01-16 13:35:29 +0530

    Updated Commit

--- 19-OOPs.cpp
+++ 19-OOPs.cpp
@@ -1,17 +1,27 @@
+// #include<iostream>
+// using namespace std;
+// class Arpita{
+//     public:
+//     int Hello;
+//     char Hello2;
+//     float Hello3;
+// };
+// int main(){
+//     Arpita A1;
+//     Arpita A2;
+//     Arpita A3;
+//     cout<<"size: " <<sizeof(A1.Hello)<<endl;
+//     cout<<"size: " <<sizeof(A2.Hello2)<<endl;
+//     cout<<"size: " <<sizeof(A3.Hello3)<<endl;
+//     return 0;
+// }
+
+
+//access modifier: public; => can be accessed anywhere in the program  
+//                 private; => it is applied in default
+//                 protected; => can be accessed in the class and the child cla ss 
 #include<iostream>
 using namespace std;
-class Arpita{
-    public:
-    int Hello;
-    char Hello2;
-    float Hello3;
-};
 int main(){
-    Arpita A1;
-    Arpita A2;
-    Arpita A3;
-    cout<<"size: " <<sizeof(A1.Hello)<<endl;
-    cout<<"size: " <<sizeof(A2.Hello2)<<endl;
-    cout<<"size: " <<sizeof(A3.Hello3)<<endl;
     return 0;
 }
