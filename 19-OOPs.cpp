6eb0a4cb3d01058ae19242a8caf5191517c11551
ArpitaAgrahari <arpitaagrahari004@gmail.com>
2024-01-16 13:47:58 +0530

    Updated Commit

--- 19-OOPs.cpp
+++ 19-OOPs.cpp
@@ -19,9 +19,45 @@
 
 //access modifier: public; => can be accessed anywhere in the program  
 //                 private; => it is applied in default
-//                 protected; => can be accessed in the class and the child cla ss 
-#include<iostream>
-using namespace std;
-int main(){
-    return 0;
-}
+//                 protected; => can be accessed in the class and the child class
+
+
+// Getters and Setters in C++:
+// Getter : It is a method that is used to get the value of variable which is private or to fetch data from private.
+// Setter : It is a method that is used to set the conditions of private class fucntion .
+// #include<iostream>
+// using namespace std;
+// class Hero{
+//     private:
+//     int Health;
+//      public:
+//      char level;
+//      void print(){
+//         cout<<level<<endl;
+//      }
+//      int getHealth(){
+//          return health;
+//      }
+//      char getLevel(){
+//          return level;
+//      }
+//      void setHealth(int h){
+//          health = h;
+//      }
+//         void setLevel(char l){
+//             level = ch;
+//         }
+// }
+// int main(){
+//     Hero ramesh;
+//     cout<<"Health: "<<ramesh.getHealth()<<endl;
+//     cout<<"Level: "<<ramesh.getLevel()<<endl;
+//     ramesh.level = 'A';
+//     ramesh.Health = 70;
+
+//     cout<<"Health: "<<ramesh.getHealth()<<endl;
+//     cout<<"Level: "<<ramesh.level<<endl;
+//     return 0;
+// }
+
+
