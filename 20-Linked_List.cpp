004b8f54c6982df194777c36948e3f95c80185d6
ArpitaAgrahari <arpitaagrahari004@gmail.com>
2024-01-21 19:33:26 +0530

    Updated Commit

--- 20-Linked_List.cpp
+++ 20-Linked_List.cpp
@@ -4,20 +4,27 @@
 //Circular linked list
 // Circular doubly linked list
 
-#include<iostream>
-using namespace std;
-class LLNode{
-    public:
-        int data;
-        LLNode* next;
 
-    LLNode(int data){
-        this->data = data;
-        this->next = NULL;
-    }
-};
-int main(){
-    LLNode* node1 = new LLNode(10);
-    cout<<node1->data<<endl;
-    cout<<node1->next<<endl;
-}
+
+//Basic Pseudo Code
+
+// #include<iostream>
+// using namespace std;
+// class LLNode{
+//     public:
+//         int data;
+//         LLNode* next;
+//     //constructor
+//     LLNode(int data){
+//         this->data = data;
+//         this->next = NULL;
+//     }
+// };
+// int main(){
+//     LLNode* node1 = new LLNode(10);
+//     cout<<node1->data<<endl;
+//     cout<<node1->next<<endl;
+// }
+
+
+//Insertion of SIngly LL
