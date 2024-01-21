fe56728e21536cc55a2d231e54b17b6cb3f8ad0e
ArpitaAgrahari <arpitaagrahari004@gmail.com>
2024-01-21 19:40:40 +0530

    Updated Commit

--- 20-Linked_List.cpp
+++ 20-Linked_List.cpp
@@ -28,3 +28,34 @@
 
 
 //Insertion of SIngly LL
+
+#include<iostream>
+using namespace std;
+class Node{
+    public:
+        int data;
+        Node* next;
+    
+    Node(int data){
+        this->data = data;
+        this->next = NULL;
+    }
+};
+
+void InsertionAtHead(Node* &head,int d){
+    //new nod creation
+    Node* temp = new Node(d);
+    temp->next = head;
+    head = temp;
+}
+void print(node* &head){
+    Node* temp = head;
+    while(temp!=NULL){
+        cout<<temp<<" "<<endl;
+        temp = temp->next;
+    }
+    cout<<endl;
+}
+int main(){
+    Node* node1 = new Node(10);
+}
