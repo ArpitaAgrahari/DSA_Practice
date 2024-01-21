aaf284321e8b7e4b6990ae7d6d7aa1588db77f6d
ArpitaAgrahari <arpitaagrahari004@gmail.com>
2024-01-21 19:28:41 +0530

    Updated Commit

--- 20-Linked_List.cpp
+++ 20-Linked_List.cpp
@@ -10,9 +10,14 @@ class LLNode{
     public:
         int data;
         LLNode* next;
+
+    LLNode(int data){
+        this->data = data;
+        this->next = NULL;
+    }
 };
 int main(){
-    LLNode* node1 = new LLNode();
+    LLNode* node1 = new LLNode(10);
     cout<<node1->data<<endl;
     cout<<node1->next<<endl;
 }
