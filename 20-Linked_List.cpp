332d5277950b64a969a86e24a4cf8c61182be70f
ArpitaAgrahari <arpitaagrahari004@gmail.com>
2024-01-21 19:46:13 +0530

    Updated Commit

--- 20-Linked_List.cpp
+++ 20-Linked_List.cpp
@@ -29,38 +29,41 @@
 
 //Insertion of SIngly LL
 
-#include<iostream>
-using namespace std;
-class Node{
-    public:
-        int data;
-        Node* next;
+// #include<iostream>
+// using namespace std;
+// class Node{
+//     public:
+//         int data;
+//         Node* next;
     
-    Node(int data){
-        this->data = data;
-        this->next = NULL;
-    }
-};
+//     Node(int data){
+//         this->data = data;
+//         this->next = NULL;
+//     }
+// };
 
-void InsertionAtHead(Node* &head,int d){
-    //new nod creation
-    Node* temp = new Node(d);
-    temp->next = head;
-    head = temp;
-}
-void print(Node* &head){
-    Node* temp = head;
-    while(temp!=NULL){
-        cout<<temp<<" "<<endl;
-        temp = temp->next;
-    }
-    cout<<endl;
-}
-int main(){
-    Node* node1 = new Node(10);
+// void InsertionAtHead(Node* &head,int d){
+//     //new nod creation
+//     Node* temp = new Node(d);
+//     temp->next = head;
+//     head = temp;
+// }
+// void print(Node* &head){
+//     Node* temp = head;
+//     while(temp!=NULL){
+//         cout<<temp<<" ";
+//         temp = temp->next;
+//     }
+//     cout<<endl;
+// }
+// int main(){
+//     Node* node1 = new Node(10);
     
-    Node* head = node1;
-    print(head);
-    InsertionAtHead(head,12);
-    print(head); 
-}
+//     Node* head = node1;
+//     print(head);
+//     InsertionAtHead(head,12);
+//     print(head); 
+//     return 0;
+// }
+
+
