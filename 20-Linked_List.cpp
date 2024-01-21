477b501ce9fb654f67efb992f3e89687fdb021ba
ArpitaAgrahari <arpitaagrahari004@gmail.com>
2024-01-21 19:25:16 +0530

    Updated Commit

---
+++ 20-Linked_List.cpp
@@ -0,0 +1,14 @@
+//Types of lInked list:
+// Singly Linked List
+//Doubly Linked List
+//Circular linked list
+// Circular doubly linked list
+
+#include<iostream>
+using namespace std;
+class LLNode{
+    
+};
+int main(){
+
+}
